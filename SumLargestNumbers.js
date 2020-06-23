d647960528fbc5ee644cf8f98717f32503842370
kutluduman <kutluduman@gmail.com>
2020-06-23 15:48:22 -0400

    Initialization of Kata

---
+++ SumLargestNumbers.js
@@ -0,0 +1,16 @@
+const sumLargestNumbers = data => {
+
+const arr = data.sort((a,b)=> b-a);
+
+return arr[0]+arr[1];
+
+}
+
+const array = [65,1,6,5,7,8,9,45];
+
+console.log(sumLargestNumbers(array));
+
+
+
+
+
