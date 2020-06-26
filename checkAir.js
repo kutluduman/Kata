e573046a5d0dca2c243c51d72639dbdccfa0967b
kutluduman <kutluduman@gmail.com>
2020-06-26 00:58:44 -0400

    check air exercise

---
+++ checkAir.js
@@ -0,0 +1,42 @@
+const checkAir = function (samples, threshold) {
+
+  let countClean = 0 ;
+  let countDirty = 0 ;
+ 
+for(let i = 0 ; i < samples.length ; i++) {
+
+  if(samples[i] === 'dirty') {
+    countDirty++;
+
+  } else if(samples[i] === 'clean') {
+    countClean++;
+
+  }
+
+}
+
+if(countDirty/samples.length > threshold) {
+
+  return "Polutted";
+
+}
+
+return "Clean";
+
+
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
